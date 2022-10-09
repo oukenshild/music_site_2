2fb25f0d25ba9369b625e7442420a797be7ad6af
Danil <danililukhin@yandex.ru>
2022-10-09 13:19:27 +0500

    Files updated

--- Music_site.sql
+++ Music_site.sql
@@ -1,45 +1,45 @@
 CREATE TABLE IF NOT EXISTS Genres(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL
+	ID SERIAL PRIMARY KEY,
+	Name VARCHAR(40) UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Executors(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL
+	ID SERIAL PRIMARY KEY,
+	Name VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Albums(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL,
-	"year of issue" SERIAL NOT NULL
+	ID SERIAL PRIMARY KEY,
+	Name VARCHAR(40) NOT NULL,
+	"Year of issue" SERIAL CHECK("Year of issue" < 2023)
 );
 
 CREATE TABLE IF NOT EXISTS Tracks(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL,
-	duration INTEGER NOT null
+	ID INTEGER PRIMARY KEY REFERENCES Albums(ID),
+	Name VARCHAR(40) NOT NULL,
+	Duration TIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Compilations(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL,
-	"year of issue" SERIAL NOT NULL
+	ID SERIAL PRIMARY KEY,
+	Name VARCHAR(40) NOT NULL,
+	"Year of issue" SERIAL CHECK("Year of issue" < 2023)
 );
 
 CREATE TABLE IF NOT EXISTS GenresExecutors(
-	genre_id integer references Genres(id),
-	executor_id integer references Executors(id),
-	constraint pk primary key (genre_id, executor_id)
+	ID SERIAL PRIMARY KEY,
+	Genre_ID INTEGER NOT NULL REFERENCES Genres(ID),
+	Executor_ID INTEGER NOT NULL REFERENCES Executors(ID)
 );
 
 CREATE TABLE IF NOT EXISTS ExecutorsAlbums(
-	executor_id integer references Executors(id),	
-	album_id integer references Albums(id),
-	constraint pk primary key (executor_id, album_id)
+	ID SERIAL PRIMARY KEY,
+	Executor_ID INTEGER NOT NULL REFERENCES Executors(ID),
+	Album_ID INTEGER NOT NULL REFERENCES Albums(ID)
 );
 
 CREATE TABLE IF NOT EXISTS TracksCompilations(
-	track_id integer references Tracks(id),
-	compilation_id integer references Compilations(id),
-	constraint pk primary key (track_id, compilation_id)
+	ID SERIAL PRIMARY KEY,
+	Track_ID INTEGER NOT NULL REFERENCES Tracks(ID),
+	Compilation_ID INTEGER NOT NULL REFERENCES Compilations(ID)
 );
